aab018fc001aeff6da19fee0b65e4987e17dcd94
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-02 21:54:43 +0530

    Added :write junit test to validate the User Entry

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,7 +1,49 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to user registration Test");
+    UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+    public void validateFirstNameTest() {
+        boolean actual1 = userRegistration.firstName("Swapnil");
+        boolean actual2 = userRegistration.firstName("swapnil");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateLastNameTest() {
+        boolean actual1 = userRegistration.lastName("Patil");
+        boolean actual2 = userRegistration.lastName("patil");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateEmailTest() {
+        boolean actual1 = userRegistration.emailID("abc100@yahoo.com");
+        boolean actual2 = userRegistration.emailID(".Swapnil@gmail.com");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validateMobile() {
+        boolean actual1 = userRegistration.phoneNumber("+91-7350310455");
+        boolean actual2 = userRegistration.phoneNumber("2395824158");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+    }
+
+    @Test
+    public void validatePassword() {
+        boolean actual1 = userRegistration.password("Swapnil@1233");
+        boolean actual2 = userRegistration.password("swapnil");
+        Assert.assertTrue(actual1);
+        Assert.assertFalse(actual2);
+
     }
 }
